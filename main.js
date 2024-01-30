2a97860d3f83289c8a609e86961f0d910156f64a
arthur <arthurazevedods@gmail.com>
2024-01-30 18:29:37 -0300

    new quotes api

--- main.js
+++ main.js
@@ -5,22 +5,24 @@ const twitterBtn = document.querySelector('.twitter-share-button');
 const copyBtn = document.querySelector('.copied');
 
 const getQuote = async () => {
+    /*
     const res = await fetch('https://type.fit/api/quotes');
     const quotes = await res.json(); 
     const num = Math.floor(Math.random()*quotes.length);
     const item = quotes[num];
     const text = item.text;
     const authorName = item.author.split(',')[0];
-
-    quote.innerText = text;
-
-    author.innerText = authorName;
+    */
+    const res = await fetch('https://api.quotable.io/random');
+    const random_quote = await res.json();
+    quote.innerText = random_quote.content;
+    author.innerText = random_quote.author;
     twitterBtn.href = `https://twitter.com/intent/tweet?text=${text} - ${authorName}`
 };
 
-nextBtn.addEventListener('click',getQuote);
+nextBtn.addEventListener('click', getQuote);
 
-copyBtn.addEventListener('click',()=>{
+copyBtn.addEventListener('click', () => {
     navigator.clipboard.writeText(quote.innerText);
 });
 
